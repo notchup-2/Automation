50bf9f404732ae0c2b7221eebb8149e3e39c2b60
notchup-2 <63499699+notchup-2@users.noreply.github.com>
2020-06-06 19:07:06 +0530

    Create DriverDemo.java

---
+++ DriverDemo.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class webdriverdemo {
+   
+      WebDriver driver = new FirefoxDriver();
+      //Puts an Implicit wait, Will wait for 10 seconds before throwing exception
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+      
+      //Launch website
+      driver.navigate().to("http://www.google.com/");
+      
+      //Maximize the browser
+      driver.manage().window().maximize();
+      
+      
+      // Selecting the element by its xpath.
+      driver.findElement(By.xpath(".//*[@id = 'menu']/div[4]/div[3]/a")).click();
+      
+      // Enter value 10 in the first number of the percent Calculator
+      driver.findElement(By.id("hrof")).sendKeys("scripting");
+      
+      
+      // Click Calculate Button
+      driver.findElement(By.xpath(".//*[@id = 'content']/table/tbody/tr[2]/td/input[2]")).click();
+
+      
+      // Get the Result Text based on its xpath
+      String result =
+         driver.findElement(By.xpath(".//*[@id = 'content']/p[2]/font/b")).getText();
+
+      
+      // Print a Log In message to the screen
+      System.out.println(" The Result is " + result);
+      
+      //Close the Browser.
+      driver.close();
+   }
+}
